ca9456e46d84b15f269227ed3ad5df9aaafed9ee
JustCallMeMin <95668004+JustCallMeMin@users.noreply.github.com>
2023-10-01 20:01:25 +0700

    Begin Project

---
+++ Project/HouseRentManagement/HouseRentManagement/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HouseRentManagement
+{
+    public partial class Login : DevExpress.XtraEditors.XtraForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
